1a040b0b534152ccaa2d6d42dce917cf573e20f5
Vinícius Melo <viniciusmelo317@gmail.com>
2024-02-25 11:29:37 -0300

    config: criado escopo da classa HttpStatus, onde contém códigos HTTP para respostas de requisição

---
+++ src/utils/HttpStatus.js
@@ -0,0 +1,8 @@
+/* eslint-disable lines-between-class-members */
+export default class HttpStatus {
+  static OK = 200;
+  static CREATED = 201;
+  static BAD_REQUEST = 400;
+  static NOT_FOUND = 404;
+  static INTERNAL_SERVER_ERROR = 500;
+}
